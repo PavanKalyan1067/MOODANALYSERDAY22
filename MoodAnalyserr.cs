df6f15a93853be25cdc806ad8db59eeb7473ef89
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-10 14:58:48 +0530

    Added TC 1.1

--- MoodAnalyserr.cs
+++ MoodAnalyserr.cs
@@ -4,15 +4,14 @@
     {
         //constants
         private const string SAD_MESSAGE = "Iam in Sad Mood";
-        private const string HAPPY_MESSAGE = "Iam in Any Mood";
+     
 
 
         public string MoodCheck(string message)
         {
             if (message == SAD_MESSAGE)
                 return "SAD";
-            if (message == HAPPY_MESSAGE)
-                return "HAPPY";
+       
             return null;
         }
     }
